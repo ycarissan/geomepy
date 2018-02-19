2048d9a4cdb5be79b2882e8a5428fb4d4d5c5f5a
ycarissan <yannick.carissan@univ-amu.fr>
2018-02-19 13:17:34 +0100

    improve setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-"""A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
+"""Yet another program better than its name
 """
 
 # Always prefer setuptools over distutils
@@ -40,7 +36,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0',  # Required
+    version='1.0.0a1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -111,7 +107,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    packages=find_packages(),  # Required
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
