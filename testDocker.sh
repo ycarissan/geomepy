5722dbdc97357be41d325a2061a892f597323c24
ycarissan <yannick.carissan@univ-amu.fr>
2018-02-21 17:56:41 +0100

    change docker file

--- testDocker.sh
+++ testDocker.sh
@@ -1,8 +1,8 @@
 sudo systemctl start docker
 docker network create reseau
 docker create --name client2 --network reseau -it client_2 /bin/bash
-docker create --name client3 --network reseau -it client_3   /bin/bash
-docker create --name pypi   --network reseau -it --rm -h pypi.local -v /srv/pypi:/srv/pypi:rw -p 8080:80 codekoala/pypi:1.2.1
+docker create --name client3 --network reseau -it client_3 /bin/bash
+docker create --name pypi    --network reseau -it --rm -h pypi.local -v /srv/pypi:/srv/pypi:rw -p 8080:80 codekoala/pypi:1.2.1
 docker start pypi
 docker start client2
 docker start client3
